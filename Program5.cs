a24c39c9d1a59ae033f4d83a7a86c016f8f5c23e
Alessandra Lucas <95228437+AleLucasG@users.noreply.github.com>
2022-05-11 17:10:49 -0300

    Exe005

---
+++ Program5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                    Console.Write("Em que ano você nasceu: ");
+                    int anoNasc = Convert.ToInt16(Console.ReadLine());
+                    Console.WriteLine("------------------------------");
+
+                    int anoAtual = DateTime.Now.Year;
+                    Console.WriteLine($"Estamos atualmente no ano de {anoAtual}.");
+
+                    int idade = anoAtual - anoNasc;
+                    Console.WriteLine($"Se você nasceu em {anoNasc}, em {anoAtual} vai ter {idade} anos.");
+
+                    Console.ReadKey();
+                 
+        }
+    }
+}
